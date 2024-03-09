49b8e21d8a095db2bca33e018a35bf651e648f94
Abhishek Tiwari <69783564+AbhishekT-2002@users.noreply.github.com>
2024-03-09 17:36:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import requests
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,17 +8,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-# Initialize Chrome WebDriver (make sure you have chromedriver installed and in your PATH)
+# Initialize Chrome WebDriver
 driver = webdriver.Chrome()
+def get_reviews_url(title_id):
+    return f'https://www.imdb.com/title/{title_id}/reviews/'
 
-url = 'https://www.imdb.com/title/tt15239678/reviews/'
-driver.get(url)
+title_id = input('Enter the IMDb title ID: ')
+while not title_id:
+    title_id = input('Enter the IMDb title ID: ')
+
+url = get_reviews_url(title_id)
+response = requests.get(url)
+
+if response.status_code == 404:
+    print("This IMDb title ID does not exist. Please try again.")
+    exit()
+
+else:
+    # Page loaded successfully
+    print("Page loaded successfully. Proceed with the rest of your code.")
+    title_soup = BeautifulSoup(response.text, 'html.parser')
+    title = title_soup.find('a', attrs={'itemprop': 'url'}).text
+    driver.get(url)
 
 def scrape_reviews():
     review_title = []
     review_date = []
     review_content = []
     user_rating = []
+    title = None
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     reviews_divs = soup.find_all('div', class_='lister-item-content')
@@ -75,9 +94,9 @@ while True:
         break
 
 print(f'Loaded {counter} times')
+review_title, review_date, review_content, user_rating = scrape_reviews()
 driver.quit()
 
-review_title, review_date, review_content, user_rating = scrape_reviews()
 
 df = pd.DataFrame({
     'Review Title': review_title,
@@ -86,4 +105,4 @@ df = pd.DataFrame({
     'User Rating': user_rating
 })
 
-df.to_csv('dune_2_reviews.csv', index=False)
+df.to_csv(f'{title}-reviews.csv', index=False)
